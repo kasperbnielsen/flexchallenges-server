e10c311619e05ca615296d015f21ffafa83fc20a
Tobias Bøgvad Nielsen <tobi844h@edu.ucl.dk>
2023-10-13 17:17:25 +0200

    added API key

--- src/routes/leagues.ts
+++ src/routes/leagues.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 const leagueRouter = Router();
+const APIKEY = process.env.API_KEY;
+if (!APIKEY) throw new Error("API-KEY Missing!");
 
 leagueRouter.get("/:region", async (req, res) => {
-  const url = `https://${req.params.region}.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5`;
+  const url = `https://${req.params.region}.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=${APIKEY}`;
 
   const data = await fetch(url, { method: "GET" }).then((res) => res.json());
 
